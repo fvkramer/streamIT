984b3d0f731c375763e71a4abc5645f3964d5d87
FilippKramer <krame490@gmail.com>
2018-12-11 14:13:16 -0800

    Migrate user database and add model level validations

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+   def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
---
+++ frontend/components/home/feed/videos/video.jsx
@@ -0,0 +1,8 @@
+// render() {
+//     return (
+//         <div>
+//             <div className="video-display"></div>
+//             <div className="video-description"></div>
+//         </div>
+//     )
+// }
