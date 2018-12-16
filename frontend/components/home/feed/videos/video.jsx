155a919370b80661eb4bd56f309a30a88374de23
FilippKramer <krame490@gmail.com>
2018-12-15 17:36:03 -0800

    Add dynamic component rendering to front end on video display component

--- frontend/components/home/feed/videos/video.jsx
+++ frontend/components/home/feed/videos/video.jsx
@@ -3,22 +3,25 @@ import { connect } from 'react-redux';
 
 import VideoDisplay from './video_display';
 import VideoDescription from './video_description';
-
+import { fetchVideo } from '../../../../actions/video_actions'; 
 
 const msp = state => {
-  const videos = Object.values(state.entities.videos)
   return {
-    videos: videos
+    videos: state.entities.videos
   } 
 }
 
-const Video = () => {
+const mdp = dispatch => ({
+  fetchVideo: id => dispatch(fetchVideo(id))
+})
+
+const Video = (props) => {
   return ( 
     <div className="video-container">
-      <VideoDisplay />
+      <VideoDisplay videos={props.videos} fetchVideo={props.fetchVideo}/>
       <VideoDescription />
     </div>
   )
 }
 
-export default connect(msp)(Video);
+export default connect(msp, mdp)(Video);
--- frontend/components/home/feed/videos/video_display.jsx
+++ frontend/components/home/feed/videos/video_display.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
-const VideoDisplay = () => {
-  return (
-    <div className="video-display">
-      <video 
+class VideoDisplay extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.fetchVideo(14)
+  }
+
+  render() {
+    let video;
+    
+    if (this.props.videos[14] === undefined) {
+      video = null;
+    } else {
+      video = (<video
         width="210"
         height="200"
         controls
-        src="/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBIZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7cb7309caf04e9a9fa454f74d96f55b9a58d5d51/fun_fair_video"></video>
-    </div>
-  )
+        src={this.props.videos[14].src}></video>)
+    }
+    return (
+      <div className="video-display">
+          {video}
+          // "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBIZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7cb7309caf04e9a9fa454f74d96f55b9a58d5d51/fun_fair_video"
+      </div>
+    )
+  }
 }
 
 export default VideoDisplay;
