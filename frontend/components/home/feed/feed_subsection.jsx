dbc520f3fc01ab12f0f36be66ce3f98a31308880
FilippKramer <krame490@gmail.com>
2018-12-17 19:57:35 -0800

    Create dynamic youtube subsection components

--- frontend/components/home/feed/feed_subsection.jsx
+++ frontend/components/home/feed/feed_subsection.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Video from './videos/video';
 
+import { fetchUserChannel } from '../../../actions/channel_actions'; 
+import { connect } from 'react-redux';
+
+const msp = state => ({
+  channelVideos: state.entities.userChannels
+})
+
+const mdp = dispatch => ({
+  fetchUserChannel: id => dispatch(fetchUserChannel(id))
+})
+
 class FeedSubsection extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); 
+  }
+
+  componentDidMount() {
+    this.props.fetchUserChannel(this.props.channel_id);
   }
 
   render() {
+    if (this.props.channelVideos[this.props.channel_id] === undefined) {
+      return null;
+    }
+
     return (
       <div className="feed-sub-container">
         <div className="feed-title-header">
@@ -26,4 +45,4 @@ class FeedSubsection extends React.Component {
   } 
 }
 
-export default FeedSubsection;
+export default connect(msp, mdp)(FeedSubsection);
--- frontend/reducers/entities/user_channel_reducer.js
+++ frontend/reducers/entities/user_channel_reducer.js
@@ -1,9 +1,9 @@
-import { RECEIVE_A_CHANNEL } from '../../actions/channel_actions';
+import { RECEIVE_A_USER } from '../../actions/channel_actions';
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case RECEIVE_A_CHANNEL:
-      return Object.assign({}, state, { [action.channel.id]: action.channel });
+    case RECEIVE_A_USER:
+      return Object.assign({}, state, { [action.user.id]: action.user });
     default:
       return state;
   }
