1d77f8bf69b1f3f122c047ed7086a81e9f8d81d3
FilippKramer <krame490@gmail.com>
2019-01-15 10:37:34 -0800

    Refactor home page buttons to be SVGs

--- frontend/components/home/sidebar/sidebar_main.jsx
+++ frontend/components/home/sidebar/sidebar_main.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
 
-const SideBarMain = () => {
-  return (
-    <div className="sidebar-main-top">
-     <ul className="sidebar-ul-links">
-      <li><a href=""><i className="core-links-home-icon"></i><span>Home</span></a></li>
-      <li><a href=""><i className="core-links-trending-icon"></i><span>Trending</span></a></li>
-      <li><a href=""><i className="core-links-history-icon"></i><span>History</span></a></li>
-     </ul>  
-    </div>
-  )
-}
+const iTag = {
+  width: '30px',
+  height: '30px',
+};
 
-export default SideBarMain
+const homeStyle = {
+  display: 'block',
+  width: '100%',
+  height: '100%',
+  fill: '#909090',
+};
+
+const SideBarMain = () => (
+  <div className="sidebar-main-top">
+    <ul className="sidebar-ul-links">
+      <li>
+        <a href="">
+          <i style={iTag}>
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="style-scope yt-icon" style={homeStyle}>
+              <g className="style-scope yt-icon">
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8" className="style-scope yt-icon" />
+              </g>
+            </svg>
+          </i>
+          <span>Home</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="">
+          <i style={iTag}>
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="style-scope yt-icon" style={homeStyle}>
+              <g className="style-scope yt-icon">
+                <path d="M17.53 11.2c-.23-.3-.5-.56-.76-.82-.65-.6-1.4-1.03-2.03-1.66-1.46-1.46-1.78-3.87-.85-5.72-.9.23-1.75.75-2.45 1.32C8.9 6.4 7.9 10.07 9.1 13.22c.04.1.08.2.08.33 0 .22-.15.42-.35.5-.22.1-.46.04-.64-.12-.06-.05-.1-.1-.15-.17-1.1-1.43-1.28-3.48-.53-5.12C5.87 10 5 12.3 5.12 14.47c.04.5.1 1 .27 1.5.14.6.4 1.2.72 1.73 1.04 1.73 2.87 2.97 4.84 3.22 2.1.27 4.35-.12 5.96-1.6 1.8-1.66 2.45-4.3 1.5-6.6l-.13-.26c-.2-.45-.47-.87-.78-1.25zm-3.1 6.3c-.28.24-.73.5-1.08.6-1.1.38-2.2-.16-2.88-.82 1.2-.28 1.9-1.16 2.1-2.05.17-.8-.14-1.46-.27-2.23-.12-.74-.1-1.37.2-2.06.15.38.35.76.58 1.06.76 1 1.95 1.44 2.2 2.8.04.14.06.28.06.43.03.82-.32 1.72-.92 2.26z" className="style-scope yt-icon" />
+              </g>
+            </svg>
+          </i>
+
+          {/* <i className="core-links-trending-icon" /> */}
+          <span>Trending</span>
+        </a>
+
+      </li>
+      <li>
+        <a href="">
+          <i style={iTag}>
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="style-scope yt-icon" style={homeStyle}>
+              <g className="style-scope yt-icon">
+                <path d="M11.9 3.75c-4.55 0-8.23 3.7-8.23 8.25H.92l3.57 3.57.04.13 3.7-3.7H5.5c0-3.54 2.87-6.42 6.42-6.42 3.54 0 6.4 2.88 6.4 6.42s-2.86 6.42-6.4 6.42c-1.78 0-3.38-.73-4.54-1.9l-1.3 1.3c1.5 1.5 3.55 2.43 5.83 2.43 4.58 0 8.28-3.7 8.28-8.25 0-4.56-3.7-8.25-8.26-8.25zM11 8.33v4.6l3.92 2.3.66-1.1-3.2-1.9v-3.9H11z" className="style-scope yt-icon" />
+              </g>
+            </svg>
+            <span>History</span>
+          </i>
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default SideBarMain;
--- frontend/components/search_results/search_results.jsx
+++ frontend/components/search_results/search_results.jsx
@@ -1,82 +1,79 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { searchVideos } from '../../actions/search_actions';
 import { Link } from 'react-router-dom';
+import { searchVideos } from '../../actions/search_actions';
 import VideoDisplay from '../home/feed/videos/video_display';
 
 
 const msp = (state, ownProps) => ({
   videos: state.entities.searchedVideos,
-  searchQuery: ownProps.match.params.searchQuery
-})
+  searchQuery: ownProps.match.params.searchQuery,
+});
 
 const mdp = dispatch => ({
-  searchVideos: searchQuery => dispatch(searchVideos(searchQuery))
-})
+  searchVideos: searchQuery => dispatch(searchVideos(searchQuery)),
+});
 
 class SearchResults extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      searchQuery: this.props.searchQuery
-    }
+      searchQuery: this.props.searchQuery,
+    };
   }
 
   componentDidMount() {
-    this.props.searchVideos(this.state.searchQuery); 
+    this.props.searchVideos(this.state.searchQuery);
   }
 
   render() {
-    let video_objects = Object.values(this.props.videos);
+    const video_objects = Object.values(this.props.videos);
     if (video_objects.length === 0) {
-
       return null;
-    } 
-    let videos = video_objects.map(video => {
-      return (
-        <div className="video-search-results-container">
-          <Link to={`/watch/${video.id}`}>
-            <VideoDisplay video_id={video.id} image_preview={video.thumbnail} />
-          </Link>
-          <div className="video-search-description">
-            <h5>{video.title}</h5>
-            <div className="video-search-wrapper">
-              <span>{video.channel_title}  &bull;</span> <span>{video.view_count} views</span>
-            </div>
-            <p>{video.description}</p>
+    }
+    const videos = video_objects.map(video => (
+      <div className="video-search-results-container">
+        <Link to={`/watch/${video.id}`}>
+          <VideoDisplay video_id={video.id} image_preview={video.thumbnail} />
+        </Link>
+        <div className="video-search-description">
+          <h5>{video.title}</h5>
+          <div className="video-search-wrapper">
+            <span>
+              {video.channel_title}
+              {' '}
+&bull;
+            </span>
+            {' '}
+            <span>
+              {video.view_count}
+              {' '}
+views
+            </span>
           </div>
+          <p>{video.description}</p>
         </div>
-      )
-    })
-    
+      </div>
+    ));
+
     return (
       <section className="video-results-container">
         <div id="menu-subrender">
           <i>
-            <svg className="filter-svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><g>
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"></path>
-            </g></svg>
+            <svg className="filter-svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
+              <g>
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z" />
+              </g>
+            </svg>
           </i>
           <span>Filter</span>
         </div>
         {videos}
-      </section>  
-    )
+      </section>
+    );
   }
 }
 
-//Infinite Scroll
-// window.addEventListener('scroll', () => {
-//   const { innerHeight } = window;
-//   const { scrollTop, offsetHeight } = document.documentElement;
-
-//   const isBottom = (innerHeight + scrollTop) > (offsetHeight - 5);
-
-//   if (isBottom) {
-//     //do cool things like fecth more songs/pics
-//   }
-// });
-
-const SearchResultsContainer = connect(msp, mdp)(SearchResults)
+const SearchResultsContainer = connect(msp, mdp)(SearchResults);
 export default SearchResultsContainer;
