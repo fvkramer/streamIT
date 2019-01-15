596318e3c0ce378da0586b78f44195d7c7705b70
FilippKramer <krame490@gmail.com>
2019-01-15 09:29:45 -0800

    Wrap logo in React Link

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 class HomePage extends React.Component {
-
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div />
+    );
+  }
 }
--- frontend/components/home/masthead/masthead.jsx
+++ frontend/components/home/masthead/masthead.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { openModal} from '../../../actions/modal_actions';
+import { Link } from 'react-router-dom';
+import { openModal } from '../../../actions/modal_actions';
 import { logout } from '../../../actions/session_actions';
 
 import MastHeadSearchContainer from './masthead_search_container';
 
 
 const msp = state => ({
-  currentUser: state.entities.users[state.session.id]
-})
+  currentUser: state.entities.users[state.session.id],
+});
 
 const mdp = dispatch => ({
   userAuth: (
@@ -17,37 +18,40 @@ const mdp = dispatch => ({
     </button>
   ),
   signOut: (
-    <button id="youtube-signout" onClick={() => dispatch(logout())} >
+    <button id="youtube-signout" onClick={() => dispatch(logout())}>
       SIGN OUT
     </button>
-  )
+  ),
 });
 
-const MastHead = ({userAuth, signOut, currentUser}) => {
-    let button;
+const MastHead = ({ userAuth, signOut, currentUser }) => {
+  let button;
 
-    if (currentUser) {
-      button = signOut;
-    } else {
-      button = userAuth;
-    }
+  if (currentUser) {
+    button = signOut;
+  } else {
+    button = userAuth;
+  }
 
-    return (
-      <div id="masthead-container-selector" className="masthead-container">
-        <button className="masthead-nav-bars"><i className="fa fa-bars"></i></button>
-        <button className="youtube-logo"></button>
-        <MastHeadSearchContainer />
-        <div className="masthead-icons">
-          <button aria-label="Create a video or post" className="msth-create-video"></button>
-          <button aria-label="Youtube Apps" className="msth-youtube-apps"></button>
-          <button aria-label="Messages" className="msth-messages"></button>
-          <button aria-label="Settings" className="msth-settings"></button>
-        </div>
-        {/* {userAuth} */}
-        {button} 
-        {/* <a href="#" id="youtube-signin">SIGN IN</a> */}
+  return (
+    <div id="masthead-container-selector" className="masthead-container">
+      <button className="masthead-nav-bars"><i className="fa fa-bars" /></button>
+      <Link to="/">
+        <button className="youtube-logo" />
+        {' '}
+      </Link>
+      <MastHeadSearchContainer />
+      <div className="masthead-icons">
+        <button aria-label="Create a video or post" className="msth-create-video" />
+        <button aria-label="Youtube Apps" className="msth-youtube-apps" />
+        <button aria-label="Messages" className="msth-messages" />
+        <button aria-label="Settings" className="msth-settings" />
       </div>
-    )
-}
+      {/* {userAuth} */}
+      {button}
+      {/* <a href="#" id="youtube-signin">SIGN IN</a> */}
+    </div>
+  );
+};
 
 export default connect(msp, mdp)(MastHead);
