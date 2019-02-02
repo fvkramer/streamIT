4e71bfdb3fa66728c703bcd476eeda2145f9dc5e
FilippKramer <krame490@gmail.com>
2019-02-01 17:55:10 -0800

    Fix sign in css on left sidebar

--- frontend/components/home/sidebar/sidebar_sign_in.jsx
+++ frontend/components/home/sidebar/sidebar_sign_in.jsx
@@ -3,16 +3,19 @@ import { connect } from 'react-redux';
 import { openModal } from '../../../actions/modal_actions';
 
 
+const buttonStyle = {
+  padding: '0 15px 0 0',
+};
 const mdp = dispatch => ({
   signIn: (
-    <button id="youtube-signin" onClick={() => dispatch(openModal('login'))}>
+    <button id="youtube-signin" style={buttonStyle} onClick={() => dispatch(openModal('login'))}>
       SIGN IN
     </button>
-  )
-})
+  ),
+});
 
-const SideBarSignIn = ({signIn}) => {
-  let button = signIn;
+const SideBarSignIn = ({ signIn }) => {
+  const button = signIn;
   return (
     <div className="sidebar-signin-container">
       <div className="sidebar-signin-content">
@@ -20,9 +23,9 @@ const SideBarSignIn = ({signIn}) => {
           Sign in now to see your channels and recommendations!
         </p>
         {button}
-      </div>   
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default connect(null, mdp)(SideBarSignIn);
