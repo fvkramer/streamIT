d6a318fad292d81f087fc013adbca95eb534f19b
FilippKramer <krame490@gmail.com>
2019-02-01 17:41:07 -0800

    Add favicon

--- frontend/components/home/masthead/masthead_search_container.jsx
+++ frontend/components/home/masthead/masthead_search_container.jsx
@@ -4,10 +4,10 @@ import MastHeadSearch from './masthead_search';
 //
 import { searchVideos } from '../../../actions/video_actions';
 
-//need to write search api query
-//uses search term to query database for all videos
+// need to write search api query
+// uses search term to query database for all videos
 const mdp = dispatch => ({
-  searchVideos: searchQuery => dispatch(searchVideos(searchQuery))
-})
+  searchVideos: searchQuery => dispatch(searchVideos(searchQuery)),
+});
 
 export default connect(null, mdp)(MastHeadSearch);
